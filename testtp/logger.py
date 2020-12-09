93d6c726aa92e65012611a8f138a5ce74409cf0c
wyy <wuyy@ushreit.com>
2020-12-09 16:47:53 +0800

    🚧 添加handler

--- testtp/logger.py
+++ testtp/logger.py
@@ -1,5 +1,13 @@
 from loguru import logger
+import logging
 
+class PropogateHandler(logging.Handler):
+    def emit(self, record):
+        logging.getLogger(record.name).handle(record)
+
+
+logger.add(PropogateHandler(), format="{time:YYYY-MM-DD at HH:mm:ss} | {message}")
 send = logger.level("SEND", no=38, color="<yellow>", icon="⬆")
 recv = logger.level("RECV", no=38, color="<green>", icon="⬇")
+
 
